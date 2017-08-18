9e6e8d93262e15a02b13c751950870140c2dc554
FazioNico <contact@nicolasfazio.ch>
2017-08-19 00:05:57 +0200

    rmv unused files

--- src/app/graphql/mutations/user.mutation.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @Author: Nicolas Fazio <webmaster-fazio>
- * @Date:   15-08-2017
- * @Email:  contact@nicolasfazio.ch
- * @Last modified by:   webmaster-fazio
- * @Last modified time: 18-08-2017
- */
-
- 
